daf4f0f0d806ba504115ade4ad5a8c7402b61852
aniketshirodkar <shirodkar.aniket@gmail.com>
2024-10-09 08:32:45 -0500

    Basic queries

--- answers.sh
+++ answers.sh
@@ -1,10 +1,10 @@
 SELECT Name FROM Students;
 SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
+SELECT Name FROM Students WHERE Gender='F' AND Age=30;  
 SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+INSERT INTO Students VALUES(7, 'Aniket', '18', 'M', 500);
+UPDATE Students SET Points = 700 WHERE Name='Basma';
+UPDATE Students SET Points = 10 WHERE Name='Alex';
 
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
